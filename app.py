f09eddc2b73c2b7af99834fe22dc2aea446dd4af
rjrathore <139190319+rjrathore@users.noreply.github.com>
2025-12-12 00:52:52 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pickle
-
-st.title('Movie Recommendation System')
-
-option = st.selectbox(
-    'How would you like to contacted?',
-    ('Email', 'Home Phone', 'Mobile Phone')
-)
